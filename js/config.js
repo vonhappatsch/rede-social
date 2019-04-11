7565bf2d680fa4941b8e0ac4212d4a61f877c018
vonhappatsch <vonhappatsch@gmail.com>
2019-04-11 05:07:26 -0300

    organização dos arquivos em pastas

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC1jSJC_gBJigdV25aQjaP09CovBOtRCMU",
+    authDomain: "rede-social-palomita.firebaseapp.com",
+    databaseURL: "https://rede-social-palomita.firebaseio.com",
+    projectId: "rede-social-palomita",
+    storageBucket: "rede-social-palomita.appspot.com",
+    messagingSenderId: "1044699053165"
+  };
+  firebase.initializeApp(config);
